1dde8a5fab69f202f34b3b77bc03904ae11e9cfa
tinaxyfeng <tinaxyfeng@tencent.com>
2025-06-18 19:21:17 +0800

    发送普通任务

--- 02-server/002-SendTask_普通任务.go
+++ 02-server/002-SendTask_普通任务.go
@@ -1,4 +1,4 @@
-// 发送普通任务的相关方法
+// 发送普通任务
 package main
 
 import (
@@ -10,18 +10,12 @@ import (
 func main2() {
 	server := myutils.MyServer()
 
-	// 1.注册任务
-	server.RegisterTasks(map[string]interface{}{
-		"Add":      myutils.Add,
-		"Periodic": myutils.Periodic,
-	})
-
-	// 2.任务发布前的钩子函数
+	// 1.任务发布前的钩子函数
 	server.SetPreTaskHandler(func(signature *tasks.Signature) {
 		println("===pre task handler===")
 	})
 
-	// 3.发布任务
+	// 2.发布任务
 	// 创建任务签名
 	signature := &tasks.Signature{
 		Name: "Add",
@@ -34,7 +28,7 @@ func main2() {
 	}
 	server.SendTask(signature)
 
-	// 4.发布一个未注册的任务会怎样？
+	// 3.发布一个未注册的任务会怎样？
 	// 如果 worker 检查发现设置了允许忽略，那么就忽略这个任务，不再入队（当然，taskState 会一直存在于 backend 中，状态永远为 PENDING）
 	// 如果没有设置，那么任务会被 worker 取出后，又重新放回队列
 	signature2 := &tasks.Signature{
@@ -44,12 +38,12 @@ func main2() {
 	}
 	server.SendTask(signature2)
 
-	// 5.发布一个空任务会怎样？
+	// 4.发布一个空任务会怎样？
 	// 结论：相当于是未注册的任务，可以发布成功
 	// 但如果有 worker 的话，worker 会不断地取出它又放回，因此 IgnoreWhenTaskNotRegistered 默认是 false 的
 	server.SendTask(&tasks.Signature{})
 
-	// 6.创建一个 worker 去执行任务
+	// 5.创建一个 worker 去执行任务
 	// worker := server.NewWorker("myworker", 10)
 	// worker.Launch()
 }
