07ceb7c29ea29d4fe0dd87d118998bf08de1cda4
tinaxyfeng <tinaxyfeng@tencent.com>
2025-06-19 11:42:30 +0800

    注册任务的要求

--- 02-server/001-RegisterTasks.go
+++ 02-server/001-RegisterTasks.go
@@ -30,4 +30,27 @@ func main1() {
 	fmt.Println("Periodic", server.IsTaskRegistered("Periodic"))           // true
 	fmt.Println("NotRegistered", server.IsTaskRegistered("NotRegistered")) // false
 
+	// 4. 注册任务的要求！
+	// （1）第二个参数必须是函数类型
+	// （2）至少有一个返回值
+	// （3）最后一个返回值必须是 error 类型
+
+	// 注册一些不符合要求的任务，试试，会返回错误
+
+	// 4.1 第二个参数不是函数类型
+	err := server.RegisterTask("taskname", 123)
+	fmt.Println(err) // Task must be a func type
+
+	// 4.2 没有返回值
+	err = server.RegisterTask("taskname", func() {
+		fmt.Println("do something")
+	})
+	fmt.Println(err) // Task must return at least a single value
+
+	// 4.3 最后一个返回值不是 error 类型
+	err = server.RegisterTask("taskname", func() (int64, int64) {
+		return 1, 2
+	})
+	fmt.Println(err) // Last return value of a task must be error
+
 }
