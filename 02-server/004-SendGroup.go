f6f8141640cdb30a48212dadac2f75bb0f11e905
tinaxyfeng <tinaxyfeng@tencent.com>
2025-06-22 22:02:46 +0800

    新增 SendGroup 和 SendChord 示例，创建任务组和和弦，支持输出到文件

---
+++ 02-server/004-SendGroup.go
@@ -0,0 +1,45 @@
+// 发送任务组
+package main
+
+import (
+	myutils "demo/01-myutils"
+	"fmt"
+
+	"demo/sourcecode/machinery/v2/tasks"
+)
+
+func main4() {
+	server := myutils.MyServer()
+
+	// 1. 创建10个任务签名
+	signatures := make([]*tasks.Signature, 10)
+	for i := 0; i < len(signatures); i++ {
+		signatures[i] = &tasks.Signature{
+			Name: "Print",
+			Args: []tasks.Arg{
+				{
+					Type:  "string",
+					Value: "output_group.txt",
+				},
+				{
+					Type:  "string",
+					Value: fmt.Sprintf("hello world %d", i),
+				},
+			},
+		}
+	}
+
+	// 2. 创建一个 Group
+	group, err := tasks.NewGroup(signatures...)
+	if err != nil {
+		fmt.Println("创建 Group 失败", err)
+		return
+	}
+
+	// 3. 发送 Group
+	server.SendGroup(group, 10)
+
+	// 4. 创建一个 worker 去执行任务
+	worker := server.NewWorker("myworker", 10)
+	worker.Launch()
+}
---
+++ 02-server/005-SendChord.go
@@ -0,0 +1,67 @@
+// 发送任务组
+package main
+
+import (
+	myutils "demo/01-myutils"
+	"fmt"
+
+	"demo/sourcecode/machinery/v2/tasks"
+)
+
+func main() {
+	server := myutils.MyServer()
+
+	// 1. 创建10个任务签名
+	signatures := make([]*tasks.Signature, 10)
+	for i := 0; i < len(signatures); i++ {
+		signatures[i] = &tasks.Signature{
+			Name: "Print",
+			Args: []tasks.Arg{
+				{
+					Type:  "string",
+					Value: "output_chord.txt",
+				},
+				{
+					Type:  "string",
+					Value: fmt.Sprintf("hello world %d", i),
+				},
+			},
+		}
+	}
+
+	// 2. 创建 chordCallback（也是一个 signature）
+	chordCallback := &tasks.Signature{
+		Name: "Print",
+		Args: []tasks.Arg{
+			{
+				Type:  "string",
+				Value: "output_chord.txt",
+			},
+			{
+				Type:  "string",
+				Value: fmt.Sprintf("chord call back!!!"),
+			},
+		},
+	}
+
+	// 3. 创建一个 Ghord
+	// 3.1. 先创建 group
+	group, err := tasks.NewGroup(signatures...)
+	if err != nil {
+		fmt.Println("创建 Group 失败", err)
+		return
+	}
+	// 3.2. 再创建 chord
+	chord, err := tasks.NewChord(group, chordCallback)
+	if err != nil {
+		fmt.Println("创建 Chord 失败", err)
+		return
+	}
+
+	// 3. 发送 Chord
+	server.SendChord(chord, 10)
+
+	// 4. 创建一个 worker 去执行任务
+	worker := server.NewWorker("myworker", 10)
+	worker.Launch()
+}
