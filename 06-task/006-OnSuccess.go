0f98454f406aa90845372e6c2d3047cd6bd3ab43
tinaxyfeng <tinaxyfeng@tencent.com>
2025-06-24 11:37:27 +0800

    成功回调任务可以接收原任务的返回值，append 到自己的参数的最后面

--- 06-task/006-OnSuccess.go
+++ 06-task/006-OnSuccess.go
@@ -1,5 +1,6 @@
 // 任务的成功回调
-// 成功回调函数，可以有多个，执行顺序不一定
+// OnSuccess 是一个切片，所以可以同时执行多个成功回调，执行的顺序不可控
+// 主任务成功后，主任务的返回值（不包含必须传入的 err）会作为参数传入给 OnSuccess 中的 signature.Args 的最后面
 
 package main
 
@@ -9,40 +10,45 @@ import (
 	"fmt"
 )
 
+// 主任务
+func MainTask() (string, error) {
+	myutils.Log("主任务执行")
+	return "主任务返回值", nil
+}
+
+// 成功回调
+// 第一个参数用来接收主任务的返回值
+func OnSuc(msg, msgFromMain string) error {
+	myutils.Log(fmt.Sprintf("成功回调执行, msg: %s,  msgFromMain: %s", msg, msgFromMain))
+	return nil
+}
+
 func main() {
 	server := myutils.MyServer()
 
-	// 1. 创建回调任务的签名（10 个）
+	// 1. 注册任务
+	server.RegisterTask("MainTask", MainTask)
+	server.RegisterTask("OnSuc", OnSuc)
+
+	// 2. 创建回调任务的签名（10 个）
 	cb_signatures := make([]*tasks.Signature, 10)
 	for i := 0; i < len(cb_signatures); i++ {
 		cb_signatures[i] = &tasks.Signature{
-			Name: "Print",
+			Name: "OnSuc",
+			// 注意，我们只需要传入第 1 个参数
+			// 第 2 个（及之后的）参数会在主任务成功后、发送本 OnSuc 前，自动传入主任务的返回值
 			Args: []tasks.Arg{
 				{
 					Type:  "string",
-					Value: myutils.GetLogPath(),
-				},
-				{
-					Type:  "string",
-					Value: fmt.Sprintf("Onsuccess %d", i),
+					Value: fmt.Sprintf("我是 %d 号回调函数", i),
 				},
 			},
 		}
 	}
 
-	// 2. 创建主任务签名
+	// 3. 创建主任务签名
 	signature := &tasks.Signature{
-		Name: "Print",
-		Args: []tasks.Arg{
-			{
-				Type:  "string",
-				Value: myutils.GetLogPath(),
-			},
-			{
-				Type:  "string",
-				Value: "Hello world!",
-			},
-		},
+		Name: "MainTask",
 		// 设置成功回调
 		// 注意，回调函数的执行顺序不一定和切片中的顺序完全一致
 		OnSuccess: cb_signatures,
